4763abe97d93cc0230978ec068414238e5a9f930
jayantmanapure1 <jayantmanapure1@gmail.com>
2023-06-29 00:41:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+const searchButton = document.querySelector("button");
+
+searchButton.addEventListener("click", () => {
+  if (inputBox.value.trim() === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    inputBox.value = "";
+    saveData();
+  }
+});
+
+listContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData(){
+   localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+   listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
